e7b9f3c34d901cac50cac3e22035329093609954
Jack Burkhardt <mail@jackburkhardt.com>
2021-05-04 13:33:33 -0500

    whoops

--- src/main/java/com/jackburkhardt/plugins/NUSMP.java
+++ src/main/java/com/jackburkhardt/plugins/NUSMP.java
@@ -13,7 +13,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class NUSMP extends JavaPlugin {
 
     private static NUSMP instance;
-    public static WorldScalers scalarInstance = new WorldScalers();
+    public static WorldScalars scalarInstance = new WorldScalars();
 
     @Override
     public void onEnable() {
@@ -50,7 +50,7 @@ public final class NUSMP extends JavaPlugin {
         return instance;
 
     }
-    public static WorldScalers getScalarInstance() {
+    public static WorldScalars getScalarInstance() {
         return scalarInstance;
     }
 }
